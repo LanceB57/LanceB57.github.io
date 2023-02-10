667c41525ce76909f127ed56e7fb9f5ba9f71f95
LanceB57 <lance.y.bae@gmail.com>
2023-02-10 14:44:32 -0500

    Lab 4: CRUD + Deployment

--- app.js
+++ app.js
@@ -2,7 +2,19 @@
 const express = require("express");
 const logger = require("morgan");
 const app = express();
-const port = 8080;
+const moment = require("moment");
+const helmet = require("helmet");
+const port = process.env.port || 8080;
+
+// database connection
+const db = require("./db/db_pool");
+
+// ejs
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "ejs");
+
+// POST requests
+app.use( express.urlencoded({ extended: false }) );
 
 // middleware for logging requests
 app.use((logger("dev")));
@@ -12,22 +24,200 @@ app.use(express.static(`${__dirname}/public`));
 
 // home page
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`);
+    res.render("index");
 });
 
-// list
+// set up helmet middleware for protection
+app.use(helmet())
+
+
+// get list
+const read_list_info_sql = `
+    SELECT 
+        id, title, subject, priority, finish_by, due_date 
+    FROM 
+        list
+    WHERE
+        archived = 0;`;
+
 app.get("/list", (req, res) => {
-    res.sendFile(`${__dirname}/views/list.html`);
+    db.execute(read_list_info_sql, (error, results) => {
+        if(error){
+            res.status(500).send(error);
+        }
+        else{
+            res.render("list", {moment : moment, inventory : results});
+        }
+    });
 });
+
+// get archive
+const read_archived_info_sql = `
+    SELECT 
+        id, title, subject, priority, finish_by, due_date 
+    FROM 
+        list
+    WHERE
+        archived = 1;`;
+
+app.get("/archive", (req, res) => {
+    db.execute(read_archived_info_sql, (error, results) => {
+        if(error){
+            res.status(500).send(error);
+        }
+        else{
+            res.render("archive", {moment : moment, inventory : results});
+        }
+    });
+});
+
+
+
+const read_item_info_sql = `
+    SELECT 
+        id, title, subject, priority, finish_by, due_date, description, archived 
+    FROM 
+        list
+    WHERE
+        id = ?;`;
 
 // item
-app.get("/list/item", (req, res) => {
-    res.sendFile(`${__dirname}/views/item.html`);
+app.get("/list/item/:id", (req, res) => {
+    db.execute(read_item_info_sql, [req.params.id], (error, results) => {
+        if(error){
+            res.status(500).send(error);
+        }
+        else if(results.length === 0){
+            res.status(404).send(`No item found with id = '${req.params.id}'`);
+        }
+        else{
+            let data = results[0];
+            res.render("item", {item : data, moment : moment});
+        }
+    });
+});
+
+
+const delete_item_sql = `
+    DELETE
+    FROM
+        list
+    WHERE
+        id = ?;`;
+
+// delete
+app.get("/list/item/:id/delete", (req, res) => {
+    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+        if(error){
+            res.status(500).send(error);
+        }
+        else if(results.length === 0){
+            res.status(404).send(`No item found with id = '${req.params.id}'`);
+        }
+        else{
+            res.redirect("/list")
+        }
+    });
+});
+
+app.get("/list/item/:id/delete/archive", (req, res) => {
+    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+        if(error){
+            res.status(500).send(error);
+        }
+        else if(results.length === 0){
+            res.status(404).send(`No item found with id = '${req.params.id}'`);
+        }
+        else{
+            res.redirect("/archive")
+        }
+    });
+});
+
+// create new items
+const create_item_sql = `
+    INSERT INTO list
+        (title, subject, priority, finish_by, due_date, description)
+    VALUES
+        (?, ?, ?, ?, ?, ?);`;
+
+app.post("/list", (req, res) => {
+    db.execute(create_item_sql, [req.body.title, req.body.subject, req.body.priority, 
+        req.body.finish, req.body.due, req.body.description], (error, results) => {
+        if(error){
+            console.log(req);
+            res.status(500).send(error);
+        }
+        else{
+            res.redirect("/list");
+        }
+    });
+});
+
+// update item
+const update_item_sql = `
+    UPDATE list
+    SET
+        title = ?,
+        subject = ?,
+        priority = ?,
+        finish_by = ?,
+        due_date = ?,
+        description = ?
+    WHERE
+        id = ?`
+
+app.post("/list/item/:id/update", (req, res) => {
+    db.execute(update_item_sql, [req.body.title, req.body.subject, req.body.priority, 
+        req.body.finish, req.body.due, req.body.description, req.params.id], (error, results) => {
+        if(error){
+            console.log(req);
+            res.status(500).send(error);
+        }
+        else{
+            res.redirect(`/list/item/${req.params.id}`);
+        }
+    });
 });
 
 // archive
-app.get("/archive", (req, res) => {
-    res.sendFile(`${__dirname}/views/archive.html`);
+const archive_item_sql = `
+    UPDATE list
+    SET
+        archived = 1
+    WHERE
+        id = ?`
+
+app.get("/list/item/:id/archive", (req, res) => {
+    db.execute(archive_item_sql, [req.params.id], (error, results) => {
+        if(error){
+            console.log(req);
+            res.status(500).send(error);
+        }
+        else{
+            res.redirect("/list");
+        }
+    });
+});
+
+// unarchive
+const unarchive_item_sql = `
+    UPDATE list
+    SET
+        archived = FALSE
+    WHERE
+        id = ?`
+
+app.get("/list/item/:id/unarchive", (req, res) => {
+    db.execute(unarchive_item_sql, [req.params.id], (error, results) => {
+        if(error){
+            console.log(req);
+            res.status(500).send(error);
+        }
+        else{
+            res.redirect("/archive");
+        }
+    });
 });
 
 // start server
--- db/db_init.js
+++ db/db_init.js
@@ -10,7 +10,7 @@ const create_list_table_sql = `
     CREATE TABLE list (
         id INT NOT NULL AUTO_INCREMENT,
         title VARCHAR(45) NOT NULL,
-        subject VARCHAR(45) NOT NULL,
+        subject VARCHAR(45),
         priority INT,
         finish_by DATE,
         due_date DATE,
@@ -22,28 +22,24 @@ const create_list_table_sql = `
 
 db.execute(create_list_table_sql);
 
-/**** Populate the table ****/
-const insert_list_table_sql = `
-    INSERT INTO list
-        (title, subject, priority, finish_by, due_date, description)
-    VALUES
-        (?, ?, ?, ?, ?, ?);
-`
+// /**** Populate the table ****/
+// const insert_list_table_sql = `
+//     INSERT INTO list
+//         (title, subject, priority, finish_by, due_date, description)
+//     VALUES
+//         (?, ?, ?, ?, ?, ?);
+// `
 
-db.execute(insert_list_table_sql, ['Worksheet', 'Math', '1', '2023-01-20', '2023-01-23', 'Gradient Worksheet']);
+// /**** Read the table ****/
+// const read_list_table_sql = "SELECT * FROM list;";
 
-db.execute(insert_list_table_sql, ['Read', 'Personal', '3', null, null, null]);
-
-/**** Read the table ****/
-const read_list_table_sql = "SELECT * FROM list;";
-
-db.execute(read_list_table_sql,
-    (error, results) => {
-        if(error)
-            throw error;
-        console.log("Table 'list' initialized with:");
-        console.log(results);
-    }
-);
+// db.execute(read_list_table_sql,
+//     (error, results) => {
+//         if(error)
+//             throw error;
+//         console.log("Table 'list' initialized with:");
+//         console.log(results);
+//     }
+// );
 
 db.end()
